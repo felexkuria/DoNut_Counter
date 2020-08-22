ae5e3bc8d7c9c487ae8cabfd7f759577f7ed48b4
felex <engineerfelex@gmail.com>
2020-08-22 08:58:25 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: (Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              "I Am Rich",
+            ),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image.asset('images/Ring.jpg'),
+        ))),
+  ));
+}
